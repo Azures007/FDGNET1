7e0afce6c8510e19d057b992f4f6fa1faa90e57e
hhh <huihuang1366@163.com>
2025-12-29 17:57:37 +0800

    补充菜单脚本--报工记录 BOM配置文件

--- sql/菜单sql脚本.sql
+++ sql/菜单sql脚本.sql
@@ -263,3 +263,7 @@ INSERT INTO t_sys_menu (menu_id, menu_name, menu_explain, "path", flag, parent_i
 INSERT INTO t_sys_menu (menu_id, menu_name, menu_explain, "path", flag, parent_id, enabled, created_time, created_name, updated_time, updated_name, menu_type, skip_url, sort, br, menu_icon) VALUES(253, '盘点记录-审核', '', '/pd-mgt/audit', '1', 233, '1', '2025-12-01', 'tenant@thingsboard.org', '2025-12-01', 'tenant@thingsboard.org', NULL, '', 0, '盘点记录-审核', NULL);
 
 INSERT INTO t_sys_menu (menu_id, menu_name, menu_explain, "path", flag, parent_id, enabled, created_time, created_name, updated_time, updated_name, menu_type, skip_url, sort, br, menu_icon) VALUES(252, '复制', 'domain', '/recipe/copy', '1', 244, '1', '2025-11-10', 'tenant@thingsboard.org', '2025-11-10', 'tenant@thingsboard.org', NULL, '', 4, '复制', NULL);
+
+-- 2025-12-29 报工记录
+INSERT INTO t_sys_menu (menu_id, menu_name, menu_explain, "path", flag, parent_id, enabled, created_time, created_name, updated_time, updated_name, menu_type, skip_url, sort, br, menu_icon) VALUES(254, '报工记录', '', '/report-record', '0', 0, '1', NULL, NULL, '2025-12-29', 'admin', 'link', '', 152, '报工记录', NULL);
+
